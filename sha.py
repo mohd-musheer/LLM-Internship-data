6d5b85de10221fdefa4beb46aff4a1587b64903d
mohd-musheer <musheerayan@gmail.com>
2026-04-06 09:14:13 +0530

    hashlib for srcure api data transfer

--- sha.py
+++ sha.py
@@ -1,3 +1,12 @@
+import os
+import hashlib
+
+file_path = hashlib.__file__
+size_in_kb = os.path.getsize(file_path) / 1024
+print(f"The size of hashlib module is approximately {size_in_kb:.2f} KB")
+
+
+
 import hashlib
 
 message = "my name is musheer"
@@ -5,4 +14,5 @@ message = "my name is musheer"
 
 hash = hashlib.sha512(message.encode()).hexdigest()
 print(f"SHA-512 hash of '{message}': {hash}")
+
 
