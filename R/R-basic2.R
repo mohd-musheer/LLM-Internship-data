47367fd9730078ddb85a0a690af4436d300ca961
mohd-musheer <musheerayan@gmail.com>
2026-04-14 14:34:55 +0530

    r new

--- R/R-basic2.R
+++ R/R-basic2.R
@@ -15,8 +15,31 @@ vec=c(1,2,3)
 vec[1]
 
 
+x<-data.frame(
+  name=c('A','B','C'),
+  age=c(21,22,23),
+  class=c(11,12,13),
+  d=c(1,2,3)
+)
+str(x)
+x[1,1]
+x['name']
+x[1:2,]
+
+x$name
 
 
+x$class<-NULL
+x
+x<-x[-2,]
+x
+x<-x[,-2:-3]
+x
+x<-x[,-4]
 
+d<-c(1,2,3)
 
-
+x<-cbind(x,d)
+x
+v<-c(10,20,30,40,50)
+summary(v)
