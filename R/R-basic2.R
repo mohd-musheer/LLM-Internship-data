cd6b45f714461cb91753dcf3b2c109fb86a3ac3a
mohd-musheer <musheerayan@gmail.com>
2026-04-14 19:02:26 +0530

    r new

--- R/R-basic2.R
+++ R/R-basic2.R
@@ -43,3 +43,45 @@ x<-cbind(x,d)
 x
 v<-c(10,20,30,40,50)
 summary(v)
+
+marks <- c(45,50,50,60,65,70,85,20,85,90)
+
+hist(marks, breaks = 3,main='Marks',col="lightpink")
+
+marks <- c(45,50,55,60,65,70,75,80,85,90)
+
+
+mode(marks)
+
+plot(density(marks),
+     main="Density Plot",
+     col="blue")
+
+subjects <- c("Math","Science","English","Computer")
+
+marks <- c(80,75,90,85)
+
+barplot(marks,
+        names.arg=subjects,
+        main="Student Marks",
+        col="green")
+
+a<-c(1)
+for(i in 1:1000000000){
+  a[i]=1
+}
+
+
+
+
+
+
+x <- seq(-3,3,0.1)
+
+y <- dnorm(x)
+
+plot(x,y,type="l",
+     main="Normal Distribution")
+
+
+dbinom(3, size=10, prob=0.5)
